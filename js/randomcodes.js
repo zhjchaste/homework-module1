27ff362bd825f3e4a4db5c71e0a54c829839fa13
张慧洁 <zhanghuijie@zhanghuijiedeMacBook-Pro.local>
2023-05-25 20:28:22 -0700

    week4day2

--- js/randomcodes.js
+++ js/randomcodes.js
@@ -10,11 +10,36 @@ function generateCode() {
     return code;
   }
   
-  document.getElementById("codes").innerHTML = generateCode();
+  var code = generateCode();
   
-  function disableButton() {
-    document.getElementById("submit").disabled = true;
+  document.getElementById("codes").innerHTML = code;
+  
+  function disableButton(disabled) {
+    document.getElementById("submit").disabled = disabled;
+    if (disabled) {
+      document.getElementById("submit").style.backgroundColor = "rgba(73, 119, 209, 0.3)";
+      document.getElementById("submit").style.color = "rgba(255, 255, 255, 0.5)";
+    } else {
+      document.getElementById("submit").style.backgroundColor = "rgba(73, 119, 209, 1)";
+      document.getElementById("submit").style.color = "rgba(255, 255, 255, 1)";
+    }
   }
   
-  disableButton();
+  disableButton(true);
+  
+  var getCode = '';
+  var btnvalue;
+  var str = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789@#$';
+  
+  var codebox = document.getElementById("codeentered");
+  codebox.addEventListener("input", evaluateCode);
+  
+  function evaluateCode() {
+    getCode = document.getElementById("codeentered").value;
+    var charset1 = getCode.trim();
+    var charset2 = code.trim();
+    if (charset1.length === charset2.length && charset1 === charset2) {
+      disableButton(false);
+    }
+  }
   
