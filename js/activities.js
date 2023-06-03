ad09740a94e971bf92da2bcce7cecd6952f13e69
张慧洁 <zhanghuijie@zhanghuijiedeMacBook-Pro.local>
2023-06-03 11:52:47 -0700

    week5day3

--- js/activities.js
+++ js/activities.js
@@ -16,3 +16,31 @@ $(document).ready(function() {
   // Set cursor style to hand for selectable cells
   $('table tbody tr td:not(:contains("Not Available"))').css('cursor', 'pointer');
 });
+
+$(document).ready(function(){
+  $("td").click(function() {
+    var content = $(this).text();
+    var column = $(this).index(); 
+    var locations = $('thead tr th').slice(1).map(function() {
+      return $(this).text();
+    }).get();
+
+    if (content != "Not Available") {
+      $(this).toggleClass("tdhighlight");
+      if ($(this).hasClass("tdhighlight")) {
+        $('#displaySelected').css("visibility", "visible");
+        $('#displaySelected').css("margin-top", "2em");
+        var location = locations[column - 1]; 
+        $('#result').append("<p>" + content + " at " + location + "</p>"); 
+      } else {
+        $('#result p:contains(' + content + ')').remove();
+        if ($('#result').has('p').length == false) {
+          $('#displaySelected').css("visibility", "hidden");
+          $('#displaySelected').css("margin-top", "0");
+        }
+      }
+    }
+  });
+});
+
+
